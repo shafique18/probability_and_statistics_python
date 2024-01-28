1dab9551ce86582150c434e9bff29a2247c40404
shafique khan <shafique18.18@gmail.com>
2024-01-28 08:48:53 +0100

    added bar chart  and pie chart file

---
+++ src/code/data_visualization/bar_chart_pie_chart.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Matplotlib
+# Seaborn
+# Plotnine(ggplot)
+# Bokeh
+# pygal
+# Plotly
+# geoplotlib
+# Gleam
+# missingno
+# Leather
+# Altair
+# Folium
+# PyGWalker
+# pyecharts
+# plotnine
+# Holoviews
+# vispy
